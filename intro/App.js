74e79bfc82fb59e369d2adebc3ea57aec54009ee
124050586-max <124050586@upq.edu.mx>
2025-10-10 18:38:58 -0600

    asi queda nuestra app despues de los cambios

--- intro/App.js
+++ intro/App.js
@@ -1,30 +1,8 @@
 //1. imports: zona de imports
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react'
-
+import ContadorScreemn from './screens/ContadorScreen';
 //2. Main: zona de componentes
 export default function App() {
-
-  const [contador,setContador]=useState(0);
-
   return (
-    <View style={styles.container}>
-      <Text>Contador: {contador}</Text>
-      <Button title="Incrementar" onPress={()=>setContador(contador+1)}/>
-      <Button title="Quitar" onPress={()=>setContador(contador-1)}/>
-      <Button title="Reiniciar" onPress={()=>setContador(0)}/>
-      <StatusBar style="auto" />
-    </View>
+    <ContadorScreen></ContadorScreen>
   );
 }
-
-//3. zona de styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
