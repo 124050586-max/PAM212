1659afe314c3414fc7b19f4859901fdfeb97dddf
124050586-max <124050586@upq.edu.mx>
2025-11-07 18:50:45 -0600

    ultima entrega

--- Examen2P/App.js
+++ Examen2P/App.js
@@ -17,10 +17,9 @@ const ArticleCard = ({ article }) => {
       {canselable: true}
     );
   };
-
   return (
     <View style={styles.container}>
-      <Image source={{ uri: article.image }} style={styles.image} />
+    <Image source={{ uri: article.image }} style={styles.image} />
       <Text style={styles.title}>{article.title}</Text>
       <Text style={styles.date}>{article.date}</Text>
       <Text style={styles.summary}>{article.summary}</Text>
@@ -41,7 +40,7 @@ const Section = ({ title, articles }) => (
 export default function App() {
   const categories = {
     Deportes: [
-      { title: 'Final de la Liga MX', date: '07/11/2025', summary: 'El América vence al Tigres en un partido épico.', image: "./aeropuerto.jpeg"  },
+      { title: 'Final de la Liga MX', date: '07/11/2025', summary: 'El América vence al Tigres en un partido epico.',image: "./messi adios.jpeg"},
       { title: 'Messi se retira', date: '06/11/2025', summary: 'El astro argentino anuncia su retiro definitivo.', image: "./messi adios.jpeg" },
     ],
     Entretenimiento: [
@@ -53,4 +52,27 @@ export default function App() {
       { title: 'IA revoluciona la medicina', date: '06/11/2025', summary: 'Nuevos algoritmos permiten diagnósticos más precisos.', image:"./iA.jpeg" },
     ],
   };
+
+ return (
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>elaldoprogramandonolesabe</Text>
+      </View>
+      {Object.entries(categories).map(([category, articles], index) => (
+        <Section key={index} title={category} articles={articles} />
+      ))}
+    </ScrollView>
+  );
 }
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff' },
+  header: { padding: 20, backgroundColor: '#001effff' },
+  headerText: { color: '#fff', fontSize: 24, fontWeight: 'bold', textAlign: 'center' },
+  section: { padding: 10 },
+  sectionTitle: { fontSize: 20, fontWeight: 'bold', marginBottom: 10 },
+  card: { backgroundColor: '#f2f2f2', padding: 10, marginBottom: 10, borderRadius: 8 },
+  image: { width: '100%', height: 150, borderRadius: 8 },
+  title: { fontSize: 18, fontWeight: 'bold', marginTop: 5 },
+  date: { fontSize: 12, color: '#555' },
+  summary: { fontSize: 14, marginVertical: 5 },
+});
